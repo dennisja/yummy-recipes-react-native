592f9dbeae124802ea31c8022e5efb851324a03e
dennisja <dennisjjagwe@gmail.com>
2018-03-13 18:59:36 +0300

    [Feature #155942805] Add login and register screens boiler plate

--- App.jsx
+++ App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import LoginScreen from './src/screens/LoginScreen';
+import RegisterScreen from './src/screens/RegisterScreen';
 
 export default class App extends React.Component {
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <LoginScreen/>
+        <RegisterScreen />
       </View>
     );
   }
---
+++ src/screens/RegisterScreen.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text,  } from 'react-native';
+
+class RegisterScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text> Register a user </Text>
+      </View>
+    );
+  }
+}
+
+export default RegisterScreen;
